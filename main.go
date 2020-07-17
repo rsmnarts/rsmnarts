84aa68541fe8bc126fe52e2a95b2c44dfd4bbf76
rsmnarts <rismanalkarim@gmail.com>
2020-07-18 00:56:58 +0700

    fix github actions

--- main.go
+++ main.go
@@ -18,8 +18,8 @@ func main() {
 	dUP := dataCorona.Update.Penambahan
 
 	line := fmt.Sprintf("Tanggal data terakhir\t\t: %s\n", dUP.Created)
-	line += fmt.Sprintf("😷 Terkonfirmasi\t\t: %d(+%d)\n", dUT.JumlahPositif, dUP.JumlahPositif)
-	line += fmt.Sprintf("🤕 Dalam Perawatan\t\t: %d(%.2f%% dari Terkonfirmasi)\n", dUT.JumlahDirawat, math.Round(100*float64(dUT.JumlahDirawat)/float64(dUT.JumlahPositif)))
+	line += fmt.Sprintf("😷 Terkonfirmasi\t: %d(+%d)\n", dUT.JumlahPositif, dUP.JumlahPositif)
+	line += fmt.Sprintf("🤕 Dalam Perawatan\t: %d(%.2f%% dari Terkonfirmasi)\n", dUT.JumlahDirawat, math.Round(100*float64(dUT.JumlahDirawat)/float64(dUT.JumlahPositif)))
 	line += fmt.Sprintf("😊 Sembuh\t\t\t: %d(%.2f%% dari Terkonfirmasi)\n", dUT.JumlahSembuh, math.Round(100*float64(dUT.JumlahSembuh)/float64(dUT.JumlahPositif)))
 	line += fmt.Sprintf("💀 Meninggal\t\t\t: %d(%.2f%% dari Terkonfirmasi)\n", dUT.JumlahMeninggal, math.Round(100*float64(dUT.JumlahMeninggal)/float64(dUT.JumlahPositif)))
 	fmt.Print(line)
